b12cd818015be338992cc25c8aaadc888abb3117
Gabriele Mercurio <gabrielemercurio.web@gmail.com>
2020-05-21 16:54:16 +0200

    Prova-1 Bonus "Select" .change

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Con una chiamata ajax, recuperare i dischi musicali restituiti dall'api:
 // https://flynn.boolean.careers/exercises/api/array/music
 // Ciclare quindi i dischi e ottenuti e per ognuno di essi disegnare in pagina una card utilizzando handlebars.
+
 // BONUS: creare una select con i generi dei dischi musicali (pop, rock, metal, jazz), tramite la quale si possono filtrare i dischi visualizzati (ad esempio: se nella tendina si seleziona il genere "metal", nella pagina saranno mostrati solo i dischi con il genere "metal").
 
 
@@ -14,9 +15,6 @@ $(document).ready(function() {
 
             var source = $("#disc-template").html();
             var template = Handlebars.compile(source);
-
-
-// ************** si può fare il ciclo ''for in''??? DA PROVARE!!!
 
             for (var i = 0; i < response.length; i++) {
                 var titolo = response[i]['title'];
@@ -45,5 +43,29 @@ $(document).ready(function() {
         }
     });
 
+    $('#genere').change(function () {
+
+        var option = '';
+
+        $('#genere option').each(function() {
+            option += $(this).text() + ' ';
+        });
+        console.log(option);
+    });
+
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// END
